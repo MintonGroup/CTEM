9e40799a77e8f4e6ceca8eae3a8c7c4bc756ad6e
huang474 <huang474@purdue.edu>
2015-08-19 22:46:10 +0000

    Added io_read_regotrack subroutine!

---
+++ src/io/io_read_regotrack.f90
@@ -0,0 +1,126 @@
+!**********************************************************************************************************************************
+!
+!  Unit Name   : io_read_regotrack
+!  Unit Type   : subroutine
+!  Project     : CTEM
+!  Language    : Fortran 2003
+!
+!  Description : Reads in files for pre-existing terrain grids for regolith tracking material
+!
+!  Input
+!    Arguments : 
+!
+!  Output
+!    Arguments : surf : Surface expression matrix
+! 
+!  Notes       :  
+!
+!**********************************************************************************************************************************
+subroutine io_read_regotrack(user,surf)
+   use module_globals
+   use module_regolith
+   use module_io, EXCEPT_THIS_ONE => io_read_regotrack
+   implicit none
+
+   ! Arguments
+   type(usertype),intent(in) :: user
+   type(surftype),dimension(:,:),intent(out) :: surf
+
+   ! Internals
+   integer(I4B), parameter :: LUN=7
+   integer(I4B), parameter :: LUM=8
+   integer(I4B), parameter :: LUP=9
+   integer(I4B), parameter :: LUC=10
+   real(DP),dimension(user%gridsize,user%gridsize) :: regotop,melt,comp, mixdep, excavdep
+   integer(I4B),dimension(user%gridsize,user%gridsize) :: stacks_num, mix_num 
+   real(DP), dimension(:),allocatable :: regotopi,melti,compi
+   type(regolayertype) :: newlayer
+   integer(I4B) :: ioerr,i,j,k,itmp
+   integer(kind=8) :: recsize
+   real(DP) :: dtmp
+   ! Checking if it reads a pre-existing terrain right
+   !integer(kind=8) :: recsize
+   !real(DP) :: dtmp
+   !real(DP),dimension(:,:),allocatable :: surface
+      
+   ! Executable code
+   ! Open a file for obtaining the number of stacks that is stored in each linked list
+   ioerr = 0
+   recsize = sizeof(itmp) * user%gridsize * user%gridsize
+   open(LUP,file=STACKNUMFILE,status='old',form='unformatted',recl=recsize,access='direct',iostat=ioerr)
+   if (ioerr/=0) then 
+       write(*,*) 'Error! Cannot read file ',trim(adjustl(STACKNUMFILE))
+       stop
+   end if
+   read(LUP,rec=1) stacks_num
+   close(LUP)
+
+   ! Open files with mixing information
+   !open(LUP,file='mixtop.dat',status='old',form='unformatted',recl=recsize,access='direct')
+   !read(LUP,rec=1) mix_num
+   !close(LUP)
+   !recsize = sizeof(dtmp) * user%gridsize * user%gridsize
+   !open(LUP,file='mixdmap.dat',status='old',form='unformatted',recl=recsize,access='direct')
+   !read(LUP,rec=1) mixdep
+   !close(LUP)   
+   !open(LUP,file='excavdmap.dat',status='old',form='unformatted',recl=recsize,access='direct')
+   !read(LUP,rec=1) excavdep
+   !close(LUP)
+
+   ! Open files for regolith thickness/melt fraction 
+   open(LUN,file=REGOFILE,status='old',form='unformatted',iostat=ioerr)
+   if (ioerr/=0) then 
+       write(*,*) 'Error! Cannot read file ',trim(adjustl(REGOFILE))
+       stop
+   end if
+
+   open(LUM,file=MELTFILE,status='old',form='unformatted',iostat=ioerr)
+   if (ioerr/=0) then 
+       write(*,*) 'Error! Cannot read file ',trim(adjustl(MELTFILE))
+       stop
+   end if
+
+   open(LUC,file=COMPFILE,status='old',form='unformatted',iostat=ioerr)
+   if (ioerr/=0) then
+       write(*,*) 'Error! Cannot read file ',trim(adjustl(COMPFILE))
+       stop
+   end if       
+
+   ! Start pushing regolith thickness and melt fraction of each layer in FILO manner
+   do j=1,user%gridsize
+      do i=1,user%gridsize
+
+         allocate(regotopi(stacks_num(i,j)))
+         allocate(melti(stacks_num(i,j)))    
+         allocate(compi(stacks_num(i,j)))  
+      
+         do k=1,stacks_num(i,j)
+            read(LUN) regotop(i,j)
+            regotopi(k) = regotop(i,j)
+            read(LUM) melt(i,j) 
+            melti(k) = melt(i,j)
+            read(LUC) comp(i,j)
+            compi(k) = comp(i,j)
+         end do
+
+         do k=stacks_num(i,j),1,-1
+            newlayer%thickness = regotopi(k)
+            newlayer%meltfrac  = melti(k)
+            newlayer%comp = compi(k)
+            call regolith_push(surf(i,j),newlayer)
+         end do 
+
+         deallocate(regotopi,melti,compi)
+
+         !surf(i,j)%nmix = mix_num(i,j)
+         !surf(i,j)%dmix = mixdep(i,j)
+         !surf(i,j)%dexcav = excavdep(i,j)
+
+      end do
+   end do
+   close(LUN)
+   close(LUM)
+   close(LUC)
+
+   return
+end subroutine io_read_regotrack
