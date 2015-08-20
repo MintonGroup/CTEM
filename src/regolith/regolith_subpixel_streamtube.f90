d908ae2e54f7532bd4187b911ae36524bb664180
huang474 <huang474@purdue.edu>
2015-08-20 14:45:22 +0000

    Added regolith_subpixel_streamtube subroutine!

---
+++ src/regolith/regolith_subpixel_streamtube.f90
@@ -0,0 +1,141 @@
+!**********************************************************************************************************************************
+!
+!  Unit Name   : regolith_streamtube_volume
+!  Unit Type   : subroutine
+!  Project     : CTEM
+!  Language    : Fortran 2003
+!
+!  Description : Subpixel stream tube's approximation 
+!  
+!
+!  Input
+!    Arguments :   
+!
+!  Output
+!    Arguments : surf      ::  surface 
+!           
+! 
+!  Notes       :  
+!
+!**********************************************************************************************************************************
+subroutine regolith_subpixel_streamtube(user,surfi,deltar,ri,rip1,eradi,newlayer,vmare,totseb,turnover,dmix)
+!subroutine regolith_subpixel_streamtube(user,surfi,deltar,ri,rip1,eradi,newlayer,vmare,totseb,turnover)
+   use module_globals 
+   use module_regolith, EXCEPT_THIS_ONE => regolith_subpixel_streamtube
+   implicit none
+
+   ! Arguments
+   type(usertype),intent(in) :: user
+   type(surftype),intent(inout) :: surfi
+   real(DP),intent(in)            :: deltar,ri,rip1,eradi
+   type(regolayertype),intent(inout) :: newlayer
+   real(DP),intent(out)            :: vmare,totseb
+   logical, intent(inout)          :: turnover
+   real(DP),intent(inout)          :: dmix 
+
+   ! Traversing a linked list 
+   real(DP),parameter :: a = 0.936457 
+   real(DP),parameter :: b = 1.12368
+   type(regolayertype),pointer :: current
+   real(DP) :: z,zmax,zstart,zend,rlefti,rleftf,rrighti,rrightf,rc,vsgly,x
+
+   ! * Mixing 
+   real(DP) :: zmix 
+
+   ! The depth that a stream tube dips 
+   zmax = rip1/4.0_DP
+
+   current => surfi%regolayer
+   z = surfi%regolayer%thickness
+   zmix = z
+   zstart = 0.0_DP
+   zend = z 
+   vmare = 0._DP
+   totseb = 0._DP
+
+   ! Two cases: subpixel is inside the first layer, and its volume is simply the landing ejecta blanket.
+   if (zend>=zmax) then 
+      vmare = newlayer%thickness * user%pix**2 * surfi%regolayer%comp
+      totseb = newlayer%thickness * user%pix**2
+   else
+   ! The subpixel stream tube may dip deeper than the first layer. And we use the line segments approximation, but 
+   ! the layer now intersects with both sides of a stream tube, so two intersection points between the layer and the 
+   ! stream tube must be calculated. 
+   !                                                                                  
+   !-----|--------------------------------------------------------|---------------*------------------------------*--- > 0
+   !  *  |                                                        |           *                              *
+   !  ** |                                                        |    *                              *
+   !-----*--------------------------------------------------------*----------------------------*--------------------- > z
+   !    *|  *                                              *      |
+   !     |     *                                     *            |                    *
+   !     |  *      *                         *                    |            *
+   !     |                *     *     *                           |    * 
+   !     |     *                                                  |
+   !     |                                                   *    |
+   !     |         *                                   *          |
+   !     |               *                     *                  |
+   !     |                      *     *                           |
+   !^    ^                      ^                                 ^              ^
+   !0.0 rleftf                  rc                              rrighti        eradi
+
+     rlefti  = 0.0_DP
+     rleftf  = 0.0_DP
+     rrighti = eradi
+     rrightf = eradi
+     rc      = rip1 * sqrt(3.0) / 4.0
+
+     do 
+
+      ! It should hit the bottom layer before it exits, I think. 
+      if (.not. associated(current%next)) exit
+
+      if (zend<zmax) then
+ 
+         rleftf  = regolith_quadratic_func(zend,rip1,rlefti,rc,rlefti)  
+         rrighti = regolith_quadratic_func(zend,rip1,rc,rrightf,rrightf)
+         vsgly   = 0.25 * PI * deltar**2 * a**2 * eradi / b * (abs(tan(b/eradi * rleftf) + tan(b/eradi * rrightf)&
+                 - tan(b/eradi * rlefti) - tan(b/eradi * rrighti)) - abs(b/eradi * rleftf + b/eradi * rrightf &
+                 - b/eradi * rlefti - b/eradi * rrighti)) 
+       
+         vmare = vmare + vsgly * current%comp
+         totseb = totseb + vsgly
+
+         if (z > zmix) then 
+            turnover = .true.
+            dmix = dmix + vsgly / (user%pix * user%pix)
+         end if
+
+         current => current%next
+         z = z + current%thickness 
+         zstart = zend
+         zend = z
+         rlefti = rleftf
+         rrightf = rrighti
+      ! final part of a stream tube 
+      else 
+         vsgly   = 0.25 * PI * deltar**2 * a**2 * eradi / b * (abs(tan(b) - b)) 
+         vmare   = vmare + (vsgly - totseb) * current%comp
+         totseb  = vsgly 
+     
+         if (z > zmix) then
+            turnover = .true.
+            dmix = dmix + vsgly / (user%pix * user%pix)
+         end if
+
+         exit
+      end if
+     end do
+
+     call regolith_streamtube_head(user,surfi,deltar,vmare,totseb,turnover,dmix)
+     !call regolith_streamtube_head(user,surfi,deltar,vmare,totseb,turnover)
+
+   end if
+
+   x = vmare/totseb
+   if (x /= x .or. x > 1.1 .or. x < -1.e-5) then
+   write(*,*) 'Subpix',x,deltar,ri,rip1,z,zend,zmax,vmare,totseb,vsgly,current%comp,current%thickness,&
+              surfi%regolayer%thickness,surfi%regolayer%comp
+   end if
+
+   return
+end subroutine regolith_subpixel_streamtube
