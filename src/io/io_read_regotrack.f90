42bb74ded35285d9b1869e345b26ea5f5a4d7a3e
huang474 <huang474@purdue.edu>
2015-09-20 15:06:42 +0000

    Fixed a bug in io_read_regotrack.f90!!!!!! The bug is related to pointer allocation problem. Check out line 96 & 97. At least, it runs way much faster than before it was fixed.

--- src/io/io_read_regotrack.f90
+++ src/io/io_read_regotrack.f90
@@ -31,8 +31,8 @@ subroutine io_read_regotrack(user,surf)
    integer(I4B), parameter :: LUM=8
    integer(I4B), parameter :: LUP=9
    integer(I4B), parameter :: LUC=10
-   real(DP),dimension(user%gridsize,user%gridsize) :: regotop,melt,comp, mixdep, excavdep
-   integer(I4B),dimension(user%gridsize,user%gridsize) :: stacks_num, mix_num 
+   real(DP),dimension(user%gridsize,user%gridsize) :: regotop,melt,comp!, mixdep, excavdep
+   integer(I4B),dimension(user%gridsize,user%gridsize) :: stacks_num!, mix_num 
    real(DP), dimension(:),allocatable :: regotopi,melti,compi
    type(regolayertype) :: newlayer
    integer(I4B) :: ioerr,i,j,k,itmp
@@ -44,6 +44,7 @@ subroutine io_read_regotrack(user,surf)
    !real(DP),dimension(:,:),allocatable :: surface
       
    ! Executable code
+
    ! Open a file for obtaining the number of stacks that is stored in each linked list
    ioerr = 0
    recsize = sizeof(itmp) * user%gridsize * user%gridsize
@@ -55,18 +56,6 @@ subroutine io_read_regotrack(user,surf)
    read(LUP,rec=1) stacks_num
    close(LUP)
 
-   ! Open files with mixing information
-   !open(LUP,file='mixtop.dat',status='old',form='unformatted',recl=recsize,access='direct')
-   !read(LUP,rec=1) mix_num
-   !close(LUP)
-   !recsize = sizeof(dtmp) * user%gridsize * user%gridsize
-   !open(LUP,file='mixdmap.dat',status='old',form='unformatted',recl=recsize,access='direct')
-   !read(LUP,rec=1) mixdep
-   !close(LUP)   
-   !open(LUP,file='excavdmap.dat',status='old',form='unformatted',recl=recsize,access='direct')
-   !read(LUP,rec=1) excavdep
-   !close(LUP)
-
    ! Open files for regolith thickness/melt fraction 
    open(LUN,file=REGOFILE,status='old',form='unformatted',iostat=ioerr)
    if (ioerr/=0) then 
@@ -74,53 +63,53 @@ subroutine io_read_regotrack(user,surf)
        stop
    end if
 
-   open(LUM,file=MELTFILE,status='old',form='unformatted',iostat=ioerr)
-   if (ioerr/=0) then 
-       write(*,*) 'Error! Cannot read file ',trim(adjustl(MELTFILE))
-       stop
-   end if
-
    open(LUC,file=COMPFILE,status='old',form='unformatted',iostat=ioerr)
    if (ioerr/=0) then
        write(*,*) 'Error! Cannot read file ',trim(adjustl(COMPFILE))
        stop
-   end if       
+   end if 
+
+   !open(LUM,file=MELTFILE,status='old',form='unformatted',iostat=ioerr)
+   if (ioerr/=0) then 
+       write(*,*) 'Error! Cannot read file ',trim(adjustl(MELTFILE))
+       stop
+   end if   
 
    ! Start pushing regolith thickness and melt fraction of each layer in FILO manner
    do j=1,user%gridsize
       do i=1,user%gridsize
 
          allocate(regotopi(stacks_num(i,j)))
+         allocate(compi(stacks_num(i,j)))
          allocate(melti(stacks_num(i,j)))    
-         allocate(compi(stacks_num(i,j)))  
       
          do k=1,stacks_num(i,j)
             read(LUN) regotop(i,j)
-            regotopi(k) = regotop(i,j)
-            read(LUM) melt(i,j) 
-            melti(k) = melt(i,j)
+            regotopi(k) = regotop(i,j)       
             read(LUC) comp(i,j)
             compi(k) = comp(i,j)
+            read(LUM) melt(i,j) 
+            melti(k) = melt(i,j)
+            !write(*,*) i,j,k,regotopi(k)
          end do
+
+         allocate(surf(i,j)%regolayer)
+         nullify(surf(i,j)%regolayer%next)
 
          do k=stacks_num(i,j),1,-1
             newlayer%thickness = regotopi(k)
-            newlayer%meltfrac  = melti(k)
             newlayer%comp = compi(k)
+            newlayer%meltfrac  = melti(k)
             call regolith_push(surf(i,j),newlayer)
+            !write(*,*) i,j,k,surf(i,j)%regolayer%thickness
          end do 
 
-         deallocate(regotopi,melti,compi)
-
-         !surf(i,j)%nmix = mix_num(i,j)
-         !surf(i,j)%dmix = mixdep(i,j)
-         !surf(i,j)%dexcav = excavdep(i,j)
+         deallocate(regotopi,compi,melti)
 
       end do
    end do
    close(LUN)
-   close(LUM)
    close(LUC)
-
+   close(LUM)
    return
 end subroutine io_read_regotrack
