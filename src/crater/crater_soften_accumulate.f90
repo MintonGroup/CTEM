097bb1c239bfbe72bfff2204071866c9289301e9
daminton <daminton@purdue.edu>
2016-01-26 20:41:42 +0000

    Fixed per-crater diffusion bug. The kdiff accumulator was not accumulating like it was supposed to.

---
+++ src/crater/crater_soften_accumulate.f90
@@ -0,0 +1,91 @@
+!**********************************************************************************************************************************
+!
+!  Unit Name   : crater_soften_accumulate
+!  Unit Type   : subroutine
+!  Project     : CTEM
+!  Language    : Fortran 2003
+!
+!  Description : Softens the terrain of the crater based on empirical studies of equilibrium (see Minton & Fassett 2016).
+!                Accumulates the diffusivity of the grid for later application in order to improve performance
+!  
+!
+!  Input
+!    Arguments : 
+!
+!  Output
+!    Arguments :
+!           
+! 
+!  Notes       :  
+!
+!**********************************************************************************************************************************
+subroutine crater_soften_accumulate(user,surf,crater,domain,kdiff)
+   use module_globals
+   use module_util
+   use module_crater, EXCEPT_THIS_ONE => crater_soften_accumulate
+   implicit none
+
+   ! Arguments
+   type(usertype),intent(in) :: user
+   type(surftype),dimension(:,:),intent(inout) :: surf
+   type(cratertype),intent(inout) :: crater
+   type(domaintype),intent(in) :: domain
+   real(DP),dimension(:,:),intent(inout) :: kdiff
+
+   ! Internal variables
+   real(DP) :: SOFTEN_FACTOR = 1.00e-3_DP ! Constant in topographic diffusion term for crater softening
+
+   integer(I4B) :: inc,incsq,N,xpi,ypi,iradsq,i,j
+   real(DP) :: kappatmax,lrad,lradsq,xp,yp,fradsq
+
+   ! TESTING
+   !open(unit=55,file="SOFTEN_FACTOR.test",status="old")
+   !read(55,*) SOFTEN_FACTOR
+   !close(55)
+   !********
+
+
+   kappatmax = SOFTEN_FACTOR * crater%fcrat**2
+   
+   inc = int(crater%frad/user%pix*(domain%small/crater%rheight)**(-1._DP/RIMDROP)) !  Maximum distance of crater form
+   inc = max(min(max(crater%rimdispx,inc),PBCLIM*user%gridsize),1)
+   crater%maxinc = max(crater%maxinc,inc)
+   fradsq = crater%frad**2
+   incsq = inc**2
+
+   ! Loop over affected matrix area
+   do j=-inc,inc  ! Do the loop in pixel space
+      do i=-inc,inc
+         ! find distance from crater center
+         iradsq = i*i + j*j
+         if (iradsq <= incsq) then
+            ! find elevation and grid point
+            xpi = crater%xlpx + i
+            ypi = crater%ylpx + j
+
+            ! Find distance from crater center to current pixel center in real space
+            xp = xpi * user%pix
+            yp = ypi * user%pix
+            
+            lradsq = (crater%xl - xp)**2 + (crater%yl - yp)**2
+
+            ! periodic boundary conditions
+            call util_periodic(xpi,ypi,user%gridsize)
+            
+            ! interior of the crater should have a constant kappa*t, while the
+            ! rim should fall away with the power law drop as the rim profile
+            if (lradsq < fradsq) then 
+               kdiff(xpi,ypi) = kdiff(xpi,ypi) + kappatmax
+            else 
+               lrad = sqrt(lradsq)
+               kdiff(xpi,ypi) = kdiff(xpi,ypi) + kappatmax * ((crater%frad / lrad)**RIMDROP)
+            end if
+
+         end if
+
+      end do
+   end do !end area loopover 
+
+return
+end subroutine crater_soften_accumulate
+
