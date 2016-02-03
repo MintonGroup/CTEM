e205b5286bd8d3e7e2b7251c23f9ba09e604c2b4
huang474 <huang474@purdue.edu>
2016-02-03 22:09:28 +0000

    Update crater_form_interior.f90! I added two optional arguments, which serves to calculate the porous mixing region. If this subroutine in crater_emplace.f90 does not pass these two arguments or turn off the "doregotrack" flag, it won't do anything to it. Let me know if you have any issue compiling your working copy of CTEM!

--- src/crater/crater_form_interior.f90
+++ src/crater/crater_form_interior.f90
@@ -18,7 +18,7 @@
 !  Notes       :  
 !
 !**********************************************************************************************************************************
-subroutine crater_form_interior(user,surfi,crater,lradsq,newelev,melev)
+subroutine crater_form_interior(user,surfi,crater,lradsq,newelev,melev,thickness_porous_tot,thickness_porous_mare)
    use module_globals
    use module_util
    use module_regolith
@@ -32,10 +32,17 @@ subroutine crater_form_interior(user,surfi,crater,lradsq,newelev,melev)
    type(cratertype),intent(in) :: crater
    real(DP),intent(in) :: lradsq
    real(DP),intent(in) :: newelev,melev
+   real(DP),intent(inout),optional :: thickness_porous_tot, thickness_porous_mare
 
    ! Internal variables
    real(DP) :: cform,newdem,elchange,pikeD
    integer(I4B) :: layer
+
+   ! Test: internal variables of calculating transient crater's parabola shape
+   real(DP) :: trdepth, trparab, trform, trvcorr
+   real(DP), parameter :: TRDDRATIO = (1.0_DP/3.0_DP + 1.0_DP/4.0_DP) / 2.0_DP 
+   real(DP) :: porous_thick
+   real(DP) :: x_wall, z_wall, vdiff, cdepth, parabarea, parabside
 
    ! Executable code
 
@@ -63,10 +70,30 @@ subroutine crater_form_interior(user,surfi,crater,lradsq,newelev,melev)
       surfi%ejcov = max(surfi%ejcov + elchange,0.0_DP)
    end if    
      
-
+   ! Test the parabola shape of a transient crater 
+   ! TRDDRATIO: it is similar to DDRATIO, which is the ratio of transient crater's depth to the transient crater diameter
+   !            in Jay's cratering book, it is between 1/4 and 1/3. 
+   ! trdepth:  it is similar to cdepth, which is the depth of a trasient crater
+   !           trdepth = TRDDRATIO * crater%rad * 2.0
+   ! trparab:  it is similar to crater%parab, which is the coefficient that determines the shape of a parabola, depending on
+   !           the distance of a point on the wall of a crater to the center of a parabola. 
+   if (user%doregotrack) then
+      call regolith_traverse_pop(elchange,surfi)
+      cdepth = DDRATIO * crater%fcrat
+      trdepth = TRDDRATIO * crater%rad * 2.0_DP
+      trparab = trdepth / ((crater%rad)**2)
+      trvcorr = trdepth 
+      trform  = trparab * lradsq - trvcorr
+      if (trform<(-cform) .and. trform < 0._DP) then
+         porous_thick = abs(abs(trform) - abs(cform))
+         call regolith_mix_porous_regime(user,surfi,porous_thick)
+         thickness_porous_tot = thickness_porous_tot + surfi%regolayer%thickness
+         thickness_porous_mare = thickness_porous_mare + surfi%regolayer%thickness * surfi%regolayer%comp
+      end if
+   end if
 
    !do regotrack: pop stuff out
-   if (user%doregotrack) call regolith_traverse_pop(elchange,surfi)
+   !if (user%doregotrack) call regolith_traverse_pop(elchange,surfi)
 
    return
 end subroutine crater_form_interior
