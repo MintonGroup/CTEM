a586147f133a92608c925edb9fb0693b7553e22e
daminton <daminton@purdue.edu>
2016-02-23 19:51:03 +0000

    Changed the crater_soften to use globally-defined parameters instead of being set by the user

--- src/crater/crater_soften.f90
+++ src/crater/crater_soften.f90
@@ -18,7 +18,7 @@
 !  Notes       :  
 !
 !**********************************************************************************************************************************
-subroutine crater_soften(user,surf,crater,domain,kdiff)
+subroutine crater_soften(user,surf,crater,domain)
    use module_globals
    use module_util
    use module_crater, EXCEPT_THIS_ONE => crater_soften
@@ -29,31 +29,87 @@ subroutine crater_soften(user,surf,crater,domain,kdiff)
    type(surftype),dimension(:,:),intent(inout) :: surf
    type(cratertype),intent(inout) :: crater
    type(domaintype),intent(in) :: domain
-   real(DP),dimension(:,:),intent(in) :: kdiff
 
    ! Internal variables
-   real(DP),dimension(0:user%gridsize + 1,0:user%gridsize + 1) :: cumulative_elchange
-   integer(I4B),dimension(2,0:user%gridsize + 1,0:user%gridsize + 1) :: indarray
-   integer(I4B) :: i,j,xpi,ypi
+   real(DP),dimension(:,:),allocatable :: cumulative_elchange,kappat
+   integer(I4B),dimension(:,:,:),allocatable :: indarray
    integer(I4B),parameter :: MAXHITS = 1
+   integer(I4B),parameter :: DROPFAC = 20
 
-   ! Create box for soften calculation (will be no bigger than the grid itself)
-   do j = 0,user%gridsize + 1
-      do i = 0,user%gridsize + 1
-         xpi = i
-         ypi = j
-         call util_periodic(xpi,ypi,user%gridsize)
-         indarray(1,i,j) = xpi
-         indarray(2,i,j) = ypi
+  ! Internal variables
+   !real(DP) :: SOFTEN_FACTOR = 2.00e-3_DP ! Constant in topographic diffusion term for crater softening
+
+   integer(I4B) :: inc,incsq,N,xpi,ypi,iradsq,i,j
+   real(DP) :: kappatmax,lrad,lradsq,xp,yp,fradsq
+
+   ! TESTING
+   !open(unit=55,file="SOFTEN_FACTOR.test",status="old")
+   !read(55,*) SOFTEN_FACTOR
+   !close(55)
+   !********
+
+
+   kappatmax = SOFTEN_FACTOR * crater%fcrat**SOFTEN_SLOPE
+   
+   inc = max(min(crater%frimpx,PBCLIM * user%gridsize),1) + 1
+   crater%maxinc = max(crater%maxinc,inc)
+   fradsq = crater%frad**2
+   incsq = inc**2
+
+   allocate(kappat(-inc:inc,-inc:inc))
+   allocate(indarray(2,-inc:inc,-inc:inc))
+   allocate(cumulative_elchange(-inc:inc,-inc:inc))
+   cumulative_elchange = 0._DP
+   indarray = inc
+   kappat = 0.0_DP
+
+   
+   ! Loop over affected matrix area
+   do j=-inc,inc  ! Do the loop in pixel space
+      do i=-inc,inc
+         ! find distance from crater center
+         iradsq = i*i + j*j
+         if (iradsq <= incsq) then
+            ! find elevation and grid point
+            xpi = crater%xlpx + i
+            ypi = crater%ylpx + j
+
+            ! Find distance from crater center to current pixel center in real space
+            xp = xpi * user%pix
+            yp = ypi * user%pix
+            
+            lradsq = (crater%xl - xp)**2 + (crater%yl - yp)**2
+
+            ! periodic boundary conditions
+            call util_periodic(xpi,ypi,user%gridsize)
+            
+            indarray(1,i,j) = xpi
+            indarray(2,i,j) = ypi
+
+            ! interior of the crater should have a constant kappa*t, while the
+            ! rim should fall away with the power law drop as the rim profile
+            if (lradsq < fradsq) then 
+               kappat(i,j) = kappatmax
+            else 
+               kappat(i,j) = 0.0_DP
+            end if
+
+         end if
+
+      end do
+   end do !end area loopover 
+
+
+   call util_diffusion_solver(user,surf,2 * inc + 1,indarray,kappat,cumulative_elchange,maxhits)
+   do j = -inc,inc
+      do i = -inc,inc
+         xpi = indarray(1,i,j)
+         ypi = indarray(2,i,j)
+         surf(xpi,ypi)%dem = surf(xpi,ypi)%dem + cumulative_elchange(i,j)
+         surf(xpi,ypi)%ejcov = max(surf(xpi,ypi)%ejcov + cumulative_elchange(i,j),0.0_DP)
       end do
    end do
-   call util_diffusion_solver(user,surf,user%gridsize + 2,indarray,kdiff,cumulative_elchange,maxhits)
-   do j = 1,user%gridsize
-      do i = 1,user%gridsize
-         surf(i,j)%dem = surf(i,j)%dem + cumulative_elchange(i,j)
-         surf(i,j)%ejcov = max(surf(i,j)%ejcov + cumulative_elchange(i,j),0.0_DP)
-      end do
-   end do
+   deallocate(kappat,indarray,cumulative_elchange)
 return
 end subroutine crater_soften
 
