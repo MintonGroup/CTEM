c77fc96131745bf835fab69d175776583c30b754
daminton <daminton@purdue.edu>
2016-02-26 21:35:20 +0000

    Take the average elevation of 5 points within the grid, rather than simply the center of the grid

--- src/crater/crater_emplace.f90
+++ src/crater/crater_emplace.f90
@@ -33,8 +33,11 @@ subroutine crater_emplace(user,surf,crater,domain,melev,xslp,yslp)
 
    ! Internal variables
    real(DP) :: lradsq,newelev
-   integer(I4B) :: xpi,ypi,i,j,inc,incsq,iradsq
+   integer(I4B) :: xpi,ypi,i,j,k,inc,incsq,iradsq
    real(DP) :: xp,yp,fradsq
+   integer(I4B),parameter :: NAVG = 5
+   real(DP) :: avgelev
+   type(surftype),dimension(NAVG) :: surfavg
 
    ! Test: porous regime mixing
    real(DP) :: comp_porous, thickness_porous_tot, thickness_porous_mare
@@ -54,6 +57,9 @@ subroutine crater_emplace(user,surf,crater,domain,melev,xslp,yslp)
    thickness_porous_mare = 0._DP
 
    ! Loop over affected matrix area
+   !$OMP PARALLEL DO DEFAULT(PRIVATE) IF(inc > INCPAR) &
+   !$OMP SHARED(inc,fradsq,incsq,melev,xslp,yslp,thickness_porous_tot,thickness_porous_mare) &
+   !$OMP SHARED(crater,user,surf) 
    do j=-inc,inc  ! Do the loop in pixel space
       do i=-inc,inc
          ! find distance from crater center
@@ -63,29 +69,50 @@ subroutine crater_emplace(user,surf,crater,domain,melev,xslp,yslp)
             newelev = melev + ((i * xslp) + (j * yslp)) * user%pix
             xpi = crater%xlpx + i
             ypi = crater%ylpx + j
-
-            ! Find distance from crater center to current pixel center in real space
-            xp = xpi * user%pix
-            yp = ypi * user%pix
-            
-            lradsq = (crater%xl - xp)**2 + (crater%yl - yp)**2
-
+               
             ! periodic boundary conditions
             call util_periodic(xpi,ypi,user%gridsize)
 
-            ! Form interior, rim, and ejecta blanket 
-            if (lradsq < fradsq) then 
-               call crater_form_interior(user,surf(xpi,ypi),crater,lradsq,newelev,melev,&
-               thickness_porous_tot,thickness_porous_mare)
-               domain%tallycoverage = domain%tallycoverage + 1
-            else 
-               call crater_form_exterior(user,surf(xpi,ypi),crater,domain,lradsq,newelev) 
-            end if
+            ! Find distance from crater center to current pixel center in real space
+            surfavg = surf(xpi,ypi)
+            do k = 1,NAVG
+               select case(k)
+               case(1)    
+                  xp = xpi * user%pix
+                  yp = ypi * user%pix
+               case(2)
+                  xp = (xpi + 0.5_DP) * user%pix 
+                  yp = ypi * user%pix
+               case(3)
+                  xp = (xpi + 0.5_DP) * user%pix 
+                  yp = (ypi + 0.5_DP) * user%pix
+               case(4)
+                  xp = (xpi - 0.5_DP) * user%pix 
+                  yp = ypi * user%pix
+               case(5)
+                  xp = (xpi - 0.5_DP) * user%pix 
+                  yp = (ypi - 0.5_DP) * user%pix
+               end select 
+               lradsq = (crater%xl - xp)**2 + (crater%yl - yp)**2
+
+               ! Form interior, rim, and ejecta blanket 
+               if (lradsq < fradsq) then 
+                  call crater_form_interior(user,surfavg(k),crater,lradsq,newelev,melev,&
+                  thickness_porous_tot,thickness_porous_mare)
+               else 
+                  call crater_form_exterior(user,surfavg(k),crater,domain,lradsq,newelev) 
+               end if
+            end do
+            surf(xpi,ypi) = surfavg(1)
+            surf(xpi,ypi)%dem = sum(surfavg%dem) / NAVG
+            surf(xpi,ypi)%ejcov = sum(surfavg%ejcov) / NAVG
 
          end if
 
       end do
    end do !end area loopover 
+   !$OMP END PARALLEL DO
+   domain%tallycoverage = domain%tallycoverage + int(fradsq * PI / user%pix**2)
 
    ! Test: Calculate the theoretical volume difference between transient crater and final crater
    ! dV = PI/4.0 * z_intersect**2 * (R_f/DDRATIO - R_TR/TRDDRATIO)
