03bb05ba596758c76385268daf011cd6db27f2ff
huang474 <huang474@purdue.edu>
2016-03-01 18:19:24 +0000

    A double freed bug found while executing "pop" in regolith_pop.f90!!!

--- src/regolith/regolith_pop.f90
+++ src/regolith/regolith_pop.f90
@@ -31,14 +31,19 @@ subroutine regolith_pop(surf)
    type(regolayertype),pointer :: current
 
    ! Executable code
+!   if (.not. associated(surf%regolayer%next)) then
+!      write(*,*) "Error: Dug too deep. Beware of balrog."
+!   else
+!      deallocate (surf%regolayer%next)
+!      surf%regolayer => surf%regolayer%next
+!   end if
+
    current => surf%regolayer
    if (.not. associated(surf%regolayer%next)) then
       write(*,*) "Error: Dug too deep. Beware of balrog."
-      !write(*,*) surf%regolayer%thickness
-      !stop
    else
       surf%regolayer => surf%regolayer%next
-      deallocate (current)
+      !deallocate (current)
    end if
    return
 end subroutine regolith_pop
