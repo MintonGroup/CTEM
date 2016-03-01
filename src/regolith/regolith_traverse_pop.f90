b5a470a14a6964ace2d143eadac8d870b01c8968
huang474 <huang474@purdue.edu>
2016-03-01 18:20:37 +0000

    Updated regolith_traverse_pop.f90! (A better and efficient way to pop layers in a traverse way!)

--- src/regolith/regolith_traverse_pop.f90
+++ src/regolith/regolith_traverse_pop.f90
@@ -28,19 +28,19 @@ subroutine regolith_traverse_pop(elchange,surfi)
    type(surftype),intent(inout):: surfi
 
    ! Internal variables
-   type(regolayertype),pointer :: current
+   !type(regolayertype),pointer :: current
    real(DP)                    :: z,depth,dz
 
    ! Executable code
-   current => surfi%regolayer
-   depth = current%thickness
+
+   depth = surfi%regolayer%thickness
    dz = 0._DP
    z = elchange
 
    if (z < 0._DP) then
 
       do 
-       if (.not. associated(current%next)) exit
+       if (.not. associated(surfi%regolayer%next)) exit
 
        if (abs(z)<=depth) then
           dz = depth - abs(z)
@@ -55,6 +55,28 @@ subroutine regolith_traverse_pop(elchange,surfi)
       end do
       
    end if
+
+!   current => surfi%regolayer
+!   depth = current%thickness
+!   dz = 0._DP
+!   z = elchange
+!   if (z < 0._DP) then
+!      do 
+!       if (.not. associated(current%next)) exit
+!
+!       if (abs(z)<=depth) then
+!          dz = depth - abs(z)
+!          surfi%regolayer%thickness = dz
+!          exit
+!       else
+!          z = abs(z) - surfi%regolayer%thickness
+!          call regolith_pop(surfi)
+!          depth = surfi%regolayer%thickness
+!       end if
+!
+!      end do
+!      
+!   end if
       
    return
 end subroutine regolith_traverse_pop
