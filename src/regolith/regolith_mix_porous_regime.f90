5fe3f5558e44e322fd50c8a05490f2a218d575c7
huang474 <huang474@purdue.edu>
2016-03-07 19:54:33 +0000

    Updated regolith_mix_porous_regime.f90 for a simple way to push a layer!

--- src/regolith/regolith_mix_porous_regime.f90
+++ src/regolith/regolith_mix_porous_regime.f90
@@ -29,52 +29,34 @@ subroutine regolith_mix_porous_regime(user,surfi,d)
    real(DP),intent(in) :: d
 
    ! Regotrack Internals
-   type(regolayertype),pointer :: current
    type(regolayertype) :: porouslayer
-   real(DP) :: z, z0, zmare, ztot
+   real(DP) :: z, z0, ztot, zmare
          
-   current => surfi%regolayer
-   z = current%thickness
-   
-   if (z < d) then 
-      zmare = 0._DP 
-      ztot = 0._DP
-      
-      if (z < d .and. z > VSMALL) then 
-          
-         do 
-          if (.not. associated(current%next)) exit
-          if (z <= d) then
-             ztot = ztot + current%thickness
-             zmare = zmare + current%thickness * current%comp
-             current => current%next
-             z0 = z
-             z = z + current%thickness
-          else
-             ztot = ztot + (d  - z0)
-             zmare = zmare + (d - z0) * current%comp
-             exit
-          end if
-         end do 
+   z = surfi%regolayer%thickness
+   ztot  = 0._DP
+   zmare = 0._DP
+   z0    = 0._DP
 
-         porouslayer%thickness  = ztot
-         porouslayer%comp       = zmare / ztot
-         porouslayer%meltfrac   = 0.0_DP
-         call regolith_traverse_pop(-1.0_DP * ztot, surfi)
-         call regolith_push(surfi,porouslayer)
-         !write(*,*) '1',surfi%regolayer%thickness, zmare, ztot, zmare/ztot, d
-         !if (d > 100.0) write(*,*) ztot, zmare/ztot, d
-      end if
-   else 
-      ztot = d
-      zmare = d * surfi%regolayer%comp
-      porouslayer%thickness = ztot
-      porouslayer%comp      = zmare/ztot
-      porouslayer%meltfrac  = 0._DP
-      call regolith_traverse_pop(-1.0_DP * ztot, surfi)
-      call regolith_push(surfi,porouslayer)
-      !write(*,*) '2',surfi%regolayer%thickness, surfi%regolayer%comp, zmare, ztot, zmare/ztot, d
-   end if 
+   do 
+    if (.not. associated(surfi%regolayer%next)) exit
+    if (z <= d) then
+       ztot  = ztot  + surfi%regolayer%thickness
+       zmare = zmare + surfi%regolayer%thickness * surfi%regolayer%comp
+       call regolith_pop(surfi)
+       z0 = z
+       z = z + surfi%regolayer%thickness
+    else
+       ztot  = ztot  + (d - z0)
+       zmare = zmare + (d - z0) * surfi%regolayer%comp
+       call regolith_traverse_pop(-1.0_DP * (d - z0), surfi)
+       exit
+    end if
+   end do 
+
+   porouslayer%thickness  = ztot
+   porouslayer%comp       = zmare / ztot
+   porouslayer%meltfrac   = 0.0_DP
+   call regolith_push(surfi, porouslayer)
 
    return
 end subroutine regolith_mix_porous_regime
