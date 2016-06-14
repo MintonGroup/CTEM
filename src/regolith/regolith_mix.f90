4a999c9d0183f0861153f1950c5bf8a325a608a2
daminton <daminton@purdue.edu>
2016-06-14 19:51:25 +0000

    Updated mixing calculation

--- src/regolith/regolith_mix.f90
+++ src/regolith/regolith_mix.f90
@@ -18,6 +18,7 @@
 !**********************************************************************************************************************************
 subroutine regolith_mix(surfi,mixing_depth)
    use module_globals
+   use modele_util
    use module_regolith, EXCEPT_THIS_ONE => regolith_mix
    implicit none
 
@@ -27,11 +28,30 @@ subroutine regolith_mix(surfi,mixing_depth)
 
    ! Internal variables
    type(regolayertype) :: newlayer
+   type(regolisttype),pointer :: poppedlist => null()
 
    !===============================================
    ! Add up all layers' info until a desired depth
    !===============================================          
-   call regolith_traverse_pop(mixing_depth, surfi,newlayer)
-   call regolith_push(surf, newlayer)
+   call util_traverse_pop(surfi%regolayer,mixing_depth,poppedlist)
 
+   newlayer%thickness = 0.0_DP
+   newlayer%comp = 0.0_DP
+   newlayer%meltfrac = 0.0_DP
+
+   do while(associated(poppedlist))
+      newlayer%thickness = newlayer%thickness + poppedlist%regodata%thickness
+      newlayer%comp = newlayer%comp + poppedlist%regodata%thickness * poppedlist%regodata%comp       
+      newlayer%meltfrac = newlayer%meltfrac + poppedlist%regodata%thickness * poppedlist%regodata%meltfrac
+      poppedlist => poppedlist%next
+   end do
+
+   ! Get average values of composition and melt fraction
+   newlayer%comp = newlayer%comp / newlayer%thickness 
+   newlayer%meltfac = newlayer%meltfrac / newlayer%thickness 
+   
+   call util_push(surf, newlayer)
+   call util_destroy_list(poppedlist)
+
+   return
 end subroutine regolith_mix
