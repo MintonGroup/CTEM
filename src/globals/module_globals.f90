e73251357fa0e8bd5829b5ae2e2cde12fcb7daac
daminton <daminton@purdue.edu>
2016-06-14 21:13:04 +0000

    Reverted back to reasonable subpixel diffusion cadence

--- src/globals/module_globals.f90
+++ src/globals/module_globals.f90
@@ -52,7 +52,7 @@ real(DP),parameter :: SMALLFAC = 1e-5_DP   ! Smallest unit of measurement propor
 integer(I4B),parameter :: MAXLAYER=20          ! Maximum number of layers (you need roughly 1-2 layers per order of magnitude of 
                                                ! resolution
 real(DP),parameter :: TALLYCOVERAGE = 0.01_DP   ! The total area coverage to reach before a tally step is executed
-real(DP),parameter :: SUBPIXELCOVERAGE = 0.000005_DP ! The total area coverage to reach before a subpixel evaluate step is executed: 0.05_DP
+real(DP),parameter :: SUBPIXELCOVERAGE = 0.05_DP ! The total area coverage to reach before a subpixel evaluate step is executed: 0.05_DP
 real(DP),parameter :: COOKIESIZE = 3.0_DP      ! Relative size of old crater to new crater that cookie cutting is applied
                                                ! Only craters smaller than COOKIESIZE times the new crater are cookie cut
 real(DP),parameter :: ALPHA = 0.125_DP
