1db29e7dd528adeb24f3dbe0f4320db6d21ed359
huang474 <huang474@purdue.edu>
2016-06-15 18:11:31 +0000

    Added util_init_list in util_traverse_pop for poppedlist.

--- src/util/util_traverse_pop.f90
+++ src/util/util_traverse_pop.f90
@@ -31,36 +31,41 @@ subroutine util_traverse_pop(regolayer,traverse_depth,poppedlist)
    ! Internal variables
    real(DP)                    :: z,depth,dz
    type(regodatatype)          :: oldregodata
+   type(regolisttype),pointer  :: current => null()
+   logical :: initstat
 
-   !=======================================
-   ! Get initial layer's info and the 
-   ! desired info that we want to modify! 
-   !=======================================
    depth = regolayer%regodata%thickness
    dz = 0._DP
    z = traverse_depth
-
-   do 
-      if (.not. associated(regolayer)) then
-         write(*,*) 'Major error in util_traverse_pop!'
-         exit
-      end if
-
-      if (z <= depth) then
-         dz = depth - z
-         regolayer%regodata%thickness = dz
-         oldregodata = regolayer%regodata
-         oldregodata%thickness = z
-         call util_push(poppedlist,oldregodata)
-         exit
-      else
-         z = z - regolayer%regodata%thickness
-         call util_pop(regolayer,oldregodata)
-         call util_push(poppedlist,oldregodata)
-         depth = regolayer%regodata%thickness
-      end if
-
-   end do
+ 
+   ! Initialize popped list
+   call util_init_list(poppedlist,initstat)
    
+   if (initstat) then
+      do 
+       if (.not. associated(regolayer)) then
+          write(*,*) 'Major error in util_traverse_pop!'
+          exit
+       end if
+
+       if (z <= depth) then
+          dz = depth - z
+          regolayer%regodata%thickness = dz
+          oldregodata = regolayer%regodata
+          oldregodata%thickness = z
+          call util_push(poppedlist,oldregodata)
+          exit
+       else
+          z = z - regolayer%regodata%thickness
+          call util_pop(regolayer,oldregodata)
+          call util_push(poppedlist,oldregodata)
+          depth = regolayer%regodata%thickness
+       end if
+      
+      end do
+   else
+      write(*,*) 'util_traverse_pop: Initialization of poppedlist failed.'
+   end if
+
    return
 end subroutine util_traverse_pop
