a6d3628a6c1013dece43d340c529058f32860247
huang474 <huang474@purdue.edu>
2016-06-15 18:12:13 +0000

    Added util_init_list subroutine in module_util!

---
+++ src/util/util_init_list.f90
@@ -0,0 +1,50 @@
+!**********************************************************************************************************************************
+!
+!  Unit Name   : util_init_list
+!  Unit Type   : function
+!  Project     : CTEM
+!  Language    : Fortran 2003
+!
+!  Description : Initialize an new list
+!  
+!
+!  Input
+!    Arguments : regolayer :: pointer to the top of the regolith stack
+!                oldlayer  :: old layer to pop off of the top of the stack
+!
+!  Output
+!    Arguments :
+!           
+! 
+!  Notes       :  
+!
+!**********************************************************************************************************************************
+subroutine util_init_list(regolayer,initstat)
+   use module_globals
+   use module_util, EXCEPT_THIS_ONE => util_init_list
+   implicit none
+
+   ! Arguments
+   type(regolisttype),pointer :: regolayer
+   logical, intent(out)       :: initstat
+
+   ! Internal variables
+   integer(I4B) :: allocstat 
+
+   ! Executable code
+   initstat = .false.
+   if (.not. associated(regolayer)) then
+      allocate(regolayer, STAT=allocstat)
+      if (allocstat == 0) then
+         initstat = .true.
+         nullify(regolayer%next)
+      else
+         write(*,*) 'util_init_list: exhausted memory.'
+      end if
+   else
+      write(*,*) 'util_init_list: Initialization went wrong. regolayer already associated.'
+   end if
+
+   return
+end subroutine util_init_list
+
