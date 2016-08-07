6072449bff0b5cc37af462832fa414d9904ab946
huang474 <huang474@purdue.edu>
2016-08-07 19:26:07 +0000

    Added a documentation of util_push subroutine!

--- src/util/util_push.f90
+++ src/util/util_push.f90
@@ -1,3 +1,33 @@
+!****f* util/util_push
+! Name
+!   util_push -- Push a new layer onto the top of an old layer. 
+! SYNOPSIS
+!   This uses 
+!   * module_globals
+!   * module_util
+!   
+!   call util_push(regolayer,newregodata)
+!
+! DESCRIPTION
+!    
+!   Push subroutine is to create new head and push it with new data onto the old layer.
+!   This subroutine will be: 
+!   * checking if the head of an input old layer is associated.
+!   * if we have enough space, allocating a new head and space for new data.  
+!   * at the end, linking new head with new data to the old head. 
+!
+! ARGUMENTS
+!   Input
+!   * regolayer   -- pointer to the top of the regolith stack
+!   * newregodata -- new regodata that is about to be pushed. 
+!   
+!   Output
+!   * regolayer   -- pointer to the top of the regolith stack with newlayer. 
+! 
+! NOTES
+! 
+!***
+
 !**********************************************************************************************************************************
 !
 !  Unit Name   : util_push
@@ -33,11 +63,6 @@ subroutine util_push(regolayer,newregodata)
    integer(I4B):: allocstat
 
    ! Executable code
-   !=======================================
-   ! Initilize the linked list 
-   !=======================================
-   ! No value in the list 
-   !=======================================
    if (associated(regolayer)) then
       allocate(newlayer, stat=allocstat)
       if (allocstat == 0) then
