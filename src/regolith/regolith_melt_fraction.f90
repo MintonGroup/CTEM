4b088cf0adc2833ba79ca2b6be5ffc0d2528d151
huang474 <huang474@purdue.edu>
2016-08-10 17:59:15 +0000

    Deleted unused module and added a documentation for regolith_melt_fraction!

--- src/regolith/regolith_melt_fraction.f90
+++ src/regolith/regolith_melt_fraction.f90
@@ -1,3 +1,42 @@
+!****f* regolith/regolith_melt_fraction
+! Name
+!   regolith_melt_fraction -- Calculate melt fraction of a stream tube 
+! SYNOPSIS
+!   This uses 
+!   * module_globals
+!   * module_regolith
+!   
+!   call regolith_melt_fraction(dimp,depthb,erad1,erad2,rmelt,meltfrac)
+!
+! DESCRIPTION
+!    
+!   This subroutine estimates a ratio of melt to the volume of a stream tube. One must be cautious about the method that
+!   we use to get a melt fraction. We assume that melt zone within a transient crater is homogeneously distributed. It is
+!   the same in any direction from an impact site. We took this advantage and *revolution* a stream tube superposed with 
+!   a vapor and melt zone. For example, two streamlines must overlap with a vapor and melt zone. To obtain melt proportion,
+!   we need to know a vapor proportion and the total volume that two streamlines revolution. We revolution the function that
+!   determines the one of two streamlines and the vapor zone and obtain the revolution volume. Then, we do the same thing 
+!   for the second streamline. At the end, the substract volume between these two revolution volume is for vapor proportion 
+!   inside these two streamlines zone. In more detail, there are three situation about intersection between two streamlines 
+!   and vapor/melt zone: 1) two streamlines are completely inside melt zone. 2) two streamlines are completely outside melt 
+!   zone. 3) two streamlines are inbetween outside and inside of melt zone. 
+! 
+! ARGUMENTS
+!   Input
+!   * dimp     -- The diameter of an impactor
+!   * depthb   -- The burial depth of a melt zone's center relaitve to the surface
+!   * erad1    -- A first streamline's emerging place from an impact site
+!   * erad2    -- A second streamline's emerging place from an impact site
+!   * rmelt    -- The radius of a melt zone
+!
+!   Output
+!   * meltfrac -- Output a final calculation of melt fraction 
+! 
+! Notes
+!   We do not consider streamlines inside a vapor zone.  
+!
+!***
+
 !**********************************************************************************************************************************
 !
 !  Unit Name   : regolith_melt_fraction
@@ -20,7 +59,6 @@
 !**********************************************************************************************************************************
 subroutine regolith_melt_fraction(dimp,depthb,erad1,erad2,rmelt,meltfrac)
    use module_globals
-   use module_util
    use module_regolith, EXCEPT_THIS_ONE => regolith_melt_fraction
    implicit none
 
