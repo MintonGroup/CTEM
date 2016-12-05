e3fc5e0924a8afb835d24f155dc71127cdc404d3
daminton <daminton@purdue.edu>
2016-12-05 20:45:39 +0000

    New model for crater form exterior

--- src/crater/crater_form_exterior.f90
+++ src/crater/crater_form_exterior.f90
@@ -44,11 +44,11 @@ subroutine crater_form_exterior(user,surfi,crater,domain,lradsq,newelev,rd,delta
 
    ! change digital elevation map
    !cform = (crater%rheight * ((crater%frad / lrad)**RIMDROP))
-   cform = (crater%rheight * ((crater%frad / lrad)**rd))
+   cform = (1.30*crater%rheight * ((crater%rad / lrad)**rd))
 
    ! elevation changes
    if (lrad <= crater%frim) then
-      hcorr = belchng * ((crater%frim - lrad) / (crater%frim - crater%frad))**20
+      hcorr = belchng * ((crater%frim - lrad) / (crater%frim - crater%rad))**20
       elchange = hcorr + cform 
    else 
       elchange = cform 
