c1357163d1c158d72e4c86e6131c3b4879270822
mhirabay <mhirabay@purdue.edu>
2016-12-29 14:13:32 +0000

    Call io_write_porotrack here.

--- src/io/io_write_surf.f90
+++ src/io/io_write_surf.f90
@@ -65,6 +65,10 @@ subroutine io_write_surf(user,surf)
       !call io_write_regodist(user,surf)
       call io_write_regotrack(user,surf) 
    end if
+   
+   ! write data from porosity analysis
+   if (user%doporosity) call io_write_porotrack(user, surf)
+   
 !   if (user%docrustal_thinning) then
 !      recsize = sizeof(itmp) * user%gridsize * user%gridsize
 !      open(LUN,file=THICKFILE,status='replace',form='unformatted',recl=recsize,access='direct')
