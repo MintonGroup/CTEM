7fa53ab183b97a78220a2cfe2b9d13b640b0b32a
daminton <daminton@purdue.edu>
2017-02-16 19:17:16 +0000

    changed critical slope routine slightly to use lrad instead of irad, though it is not currently being used.

--- src/crater/crater_critical_slope.f90
+++ src/crater/crater_critical_slope.f90
@@ -16,7 +16,7 @@
 !**********************************************************************************************************************************
 
 
-function crater_critical_slope(user,crater,iradsq) result(critical)
+function crater_critical_slope(user,crater,lrad) result(critical)
    use module_globals
    use module_util
    use module_crater, EXCEPT_THIS_ONE => crater_critical_slope
@@ -25,17 +25,17 @@ function crater_critical_slope(user,crater,iradsq) result(critical)
   ! Arguments
    type(usertype),intent(in) :: user
    type(cratertype),intent(in) :: crater
-   integer(I4B),intent(in) :: iradsq
+   real(DP),intent(in) :: lrad
    real(DP) :: critical
 
    ! Internal variables
    real(DP) :: r
 
-   r = sqrt(iradsq * 1.0_DP) / (crater%fradpx * 1.0_DP)
+   r = lrad / crater%frad 
    if (r < 0.2_DP) then
       critical = 0.0_DP
    else if (r < 0.98_DP) then
-      critical =  abs(0.228_DP + 2 * 0.083_DP * r - 3 * 0.039_DP * r**2)
+      critical =  max(abs(0.228_DP + 2 * 0.083_DP * r - 3 * 0.039_DP * r**2),CRITSLP)
    else 
       critical =  max(abs(0.187_DP - 2 * 0.018_DP * r - 3 * 0.015_DP * r**2),CRITSLP)
    end if
