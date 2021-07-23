d681cc45c57764271acfcaac39c842b4ed482d6c
David Minton <daminton@purdue.edu>
2021-07-23 14:57:11 -0400

    Fixed more bad merges

--- src/io/io_input.f90
+++ src/io/io_input.f90
@@ -77,6 +77,8 @@ subroutine io_input(infile,user)
    user%doscour = .false.
    user%tallyonly = .false.
    user%dosoftening = .true.
+   user%dorays = .false.
+   user%superdomain = .true.
    user%doregotrack = .false.
    user%basinimp = huge(0._DP)
    user%maxcrat = 1.00_DP
@@ -84,9 +86,9 @@ subroutine io_input(infile,user)
    user%testtally = .false.
    user%killatmaxcrater = .false.
    user%doporosity = .false.
-   user%soften_factor = 0.9_DP
-   user%soften_slope = 1.8_DP
-   user%soften_size  = 30.0_DP
+   user%Kd1 = 0.00_DP
+   user%psi = 2.0_DP
+   user%fe  = 1.0_DP
    user%ejecta_truncation = 10.0_DP
    user%doquasimc = .false.
    user%discontinuous = .true.
@@ -281,6 +283,16 @@ subroutine io_input(infile,user)
             call io_get_token(line, ilength, ifirst, ilast, ierr)
             token = line(ifirst:ilast)
             read(token, *) user%dosoftening
+         case ("DORAYS")
+            ifirst = ilast + 1
+            call io_get_token(line, ilength, ifirst, ilast, ierr)
+            token = line(ifirst:ilast)
+            read(token, *) user%dorays
+         case ("SUPERDOMAIN")
+            ifirst = ilast + 1
+            call io_get_token(line, ilength, ifirst, ilast, ierr)
+            token = line(ifirst:ilast)
+            read(token, *) user%superdomain
          case ("DOREGOTRACK")
             ifirst = ilast + 1
             call io_get_token(line, ilength, ifirst, ilast, ierr)
@@ -312,15 +324,15 @@ subroutine io_input(infile,user)
             token = line(ifirst:ilast)
             read(token, *) user%testtally
          case ("EJECTA_TRUNCATION") ! Radius of ejecta blankets to model in terms of crater radius
-              ifirst = ilast + 1
-              call io_get_token(line, ilength, ifirst, ilast, ierr)
-              token = line(ifirst:ilast)
-              read(token, *) user%ejecta_truncation
-         case ("DISCONTINUOUS") ! Do discontinuous ejecta blanket
             ifirst = ilast + 1
             call io_get_token(line, ilength, ifirst, ilast, ierr)
             token = line(ifirst:ilast)
-            read(token, *) user%discontinuous
+            read(token, *) user%ejecta_truncation
+         case ("DISCONTINUOUS") 
+            ifirst = ilast + 1
+            call io_get_token(line, ilength, ifirst, ilast, ierr)
+            token = line(ifirst:ilast)
+            read(token, *) user%discontinuous   
          ! Porosity model
          case ("POROSITYFLG")
             ifirst = ilast + 1
@@ -442,21 +454,21 @@ subroutine io_input(infile,user)
             token = line(ifirst:ilast)
             read(token, *) user%shadedmaxh
             read(token, *) user%shadedminh
-         case ("SOFTEN_FACTOR")
+         case ("KD1")
             ifirst = ilast + 1
             call io_get_token(line, ilength, ifirst, ilast, ierr)
             token = line(ifirst:ilast)
-            read(token, *) user%soften_factor
-         case ("SOFTEN_SLOPE")
+            read(token, *) user%Kd1
+         case ("PSI")
             ifirst = ilast + 1
             call io_get_token(line, ilength, ifirst, ilast, ierr)
             token = line(ifirst:ilast)
-            read(token, *) user%soften_slope
-         case ("SOFTEN_SIZE")
+            read(token, *) user%psi
+         case ("FE")
             ifirst = ilast + 1
             call io_get_token(line, ilength, ifirst, ilast, ierr)
             token = line(ifirst:ilast)
-            read(token, *) user%soften_size
+            read(token, *) user%fe
          !**************************************************************************
          ! The following is for backwards compatibility with older style input files
          !**************************************************************************
