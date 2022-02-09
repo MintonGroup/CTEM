a99bd2ad659fb8bf077ae71f313b9fd34918d506
David A Minton <daminton@purdue.edu>
2022-02-09 13:57:42 -0500

    Updated geometry to better separate the raised rim model and the ejecta blanket model to get better consistency

--- src/ejecta/ejecta_table_define.f90
+++ src/ejecta/ejecta_table_define.f90
@@ -75,9 +75,9 @@ subroutine ejecta_table_define(user,crater,domain,ejb,ejtble,melt)
          ! This will be replaced 
          r = lrad / crater%frad
          if (lrad >= crater%frad) then
-            thick = crater%rimheight * r**(-3.0_DP)
+            thick = crater_profile(user, crater, r) + crater%ejrim * r**(-EJPROFILE)
          else
-            thick = max(crater_profile(user,crater,r),VSMALL)
+            thick = max(crater_profile(user,crater,r) + crater%ejrim,VSMALL)
          end if
          ejb(k)%thick = log(thick) 
          ejb(k)%vesq = vejsq
