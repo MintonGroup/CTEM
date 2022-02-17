766d8a305e450ea92b1e317fc484104696b1c9af
David Minton <daminton@purdue.edu>
2022-02-17 12:17:19 -0500

    Moved Python code to correct new location

--- python/ctem/ctem/ctem_viewer_3d.py
+++ python/ctem/ctem/ctem_viewer_3d.py
@@ -95,7 +95,23 @@ if __name__ == '__main__':
     dem_vec = np.array((xvals, yvals, zvals*10)).T
     pcd = open3d.geometry.PointCloud()
     pcd.points = open3d.utility.Vector3dVector(dem_vec)
+    pcd.estimate_normals()
 
-    o3d.visualization.draw_geometries([pcd])
+    # estimate radius for rolling ball
+    distances = pcd.compute_nearest_neighbor_distance()
+    avg_dist = np.mean(distances)
+    radius = 1.5 * avg_dist
+
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
+        pcd,
+        o3d.utility.DoubleVector([radius, radius * 2]))
+
+    # create the triangular mesh with the vertices and faces from open3d
+    tri_mesh = trimesh.Trimesh(np.asarray(mesh.vertices), np.asarray(mesh.triangles),
+                               vertex_normals=np.asarray(mesh.vertex_normals))
+
+    trimesh.convex.is_convex(tri_mesh)
+
+    o3d.visualization.draw_geometries([trimesh])
     
 
