3b69dbdabd8b4bcb3324162889f6d8b6da2cfe0a
Austin Blevins <blevinsaustinm@gmail.com>
2023-06-26 11:52:08 -0400

    fixed a bug where DP numbers were given to a SP array

--- src/regolith/regolith_mix.f90
+++ src/regolith/regolith_mix.f90
@@ -45,7 +45,7 @@ subroutine regolith_mix(user,surfi,mixing_depth,domain)
 
    newlayer%thickness = 0.0_DP
    newlayer%comp      = 0.0_DP
-   newlayer%age(:)    = 0.0_DP
+   newlayer%age(:)    = 0.0_SP
    allocate(newlayer%distvol(1+domain%rcnum))
    newlayer%distvol(:) = 0.0_DP
    newlayer%ejm       = 0.0_DP
