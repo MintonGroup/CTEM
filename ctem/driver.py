c143ab0fd47281098c9dc26f45f297da6b0244de
David A Minton <daminton@purdue.edu>
2024-02-20 14:39:01 -0500

    Updated the build process so that the CTEM executable gets installed into the environment's bin folder

--- ctem/driver.py
+++ ctem/driver.py
@@ -52,14 +52,10 @@ class Simulation:
         }
 
         # Get the location of the CTEM executable
-        self.ctem_executable = Path(currentdir) / "CTEM"
-        if not self.ctem_executable.exists():
-            print(f"CTEM driver not found at {self.ctem_executable}. Trying package directory..")
-            self.ctem_executable = Path(_pyfile).parent.parent.parent / "bin" / "CTEM"
-            if not self.ctem_executable.exists():
-                warnings.warn(f"Cannot find the CTEM driver {str(self.ctem_executable)}", stacklevel=2)
-                self.ctem_executable = None
-
+        self.ctem_executable = shutil.which("CTEM")
+        if not self.ctem_executable:
+            warnings.warn(f"Cannot find the CTEM driver {str(self.ctem_executable)}", stacklevel=2)
+            self.ctem_executable = None
 
         self.user = util.read_user_input(self.user)
 
