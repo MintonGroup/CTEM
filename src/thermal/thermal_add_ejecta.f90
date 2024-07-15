e06617842a2d23e7f481136e76d187836319763b
Austin Michael Blevins <blevins2@b003.negishi.rcac.purdue.edu>
2024-07-15 16:29:03 -0400

    added untracked files necessary for thermal model

---
+++ src/thermal/thermal_add_ejecta.f90
@@ -0,0 +1,61 @@
+!**********************************************************************************************************************************
+!
+!  Unit Name   : thermal_add_ejecta.f90
+!  Unit Type   : subroutine
+!  Project     : CTEM
+!  Language    : Fortran 2003
+!
+!  Description : Adds a layer of ejecta with a given average temperature to the thermal model.
+!  
+!
+!  Input
+!    Arguments : 
+!
+!  Output
+!    Arguments :
+!           
+! 
+!  Notes       : The uppermost elevation must be the baseline for the array 
+!
+!**********************************************************************************************************************************
+subroutine thermal_add_ejecta(user,thermal,avgtemp,thickness,tx,ty)
+    use module_globals
+    use module_thermal, EXCEPT_THIS_ONE => thermal_add_ejecta
+    implicit none
+
+    ! Arguments
+    type(usertype),intent(in) :: user
+    type(thermaltype),dimension(:,:,:),intent(inout) :: thermal
+    real(DP),intent(in) :: avgtemp
+    real(DP),intent(in) :: thickness
+    integer(I4B) :: tx, ty
+
+    ! Internal variables
+    real(DP) :: voxtemp, rem
+    integer(I4B) :: npix, i
+
+    ! Executable code
+
+    if (thickness < user%zpix) then ! Ejecta layer less than z-pixel; will average
+        voxtemp = ((avgtemp * (thickness/user%zpix)) + (thermal(tx,ty,1)%temperature*((user%zpix-thickness)/(user%zpix))) / 2.0_DP)
+        thermal(tx,ty,1)%temperature = voxtemp
+    else ! Ejecta layer more than one z-pixel. Need to fill the top number of pixels with the ejecta value, then average the other one
+        npix = int(thickness / user%zpix)
+        rem = mod(thickness,user%zpix)
+        ! Shift thermal distribution down by the npix
+        do i=user%zgridsize,1,-1
+            if (i <= npix) then
+                if (i == npix) then
+                    thermal(tx,ty,i)%temperature = ((avgtemp * (rem/user%zpix)) + (thermal(tx,ty,i)%temperature*((user%zpix-thickness)/(user%zpix))) / 2.0_DP)
+                else
+                    thermal(tx,ty,i)%temperature = avgtemp
+                end if
+            else
+                thermal(tx,ty,i)%temperature = thermal(tx,ty,i-npix)%Temperature
+            end if
+        end do
+    end if
+
+    return
+end subroutine thermal_add_ejecta
+        
---
+++ src/thermal/thermal_ejecta_average.f90
@@ -0,0 +1,155 @@
+!**********************************************************************************************************************************
+!
+!  Unit Name   : thermal_ejecta_average.f90
+!  Unit Type   : subroutine
+!  Project     : CTEM
+!  Language    : Fortran 2003
+!
+!  Description : Averages non-vapor temperatures within the transient crater region of the thermal structure
+!  
+!
+!  Input
+!    Arguments :   
+!
+!  Output
+!    Arguments : 
+!           
+! 
+!  Notes       :  This is what Abramov et al. (2013) do, but might not be the best approach here. This is a first-pass exercise
+!
+!**********************************************************************************************************************************
+subroutine thermal_ejecta_average(user,surf,crater,thermal,avgtemp)
+    use module_globals
+    use module_thermal, EXCEPT_THIS_ONE => thermal_ejecta_average
+    implicit none
+
+    ! Arguments
+    type(usertype),intent(in) :: user
+    type(surftype),dimension(:,:),intent(in) :: surf
+    type(cratertype),intent(in) :: crater
+    type(thermaltype),dimension(:,:,:),intent(in) :: thermal
+    real(DP),intent(out) :: avgtemp
+
+    ! Internal variables
+    integer(I4B) :: i,j,k,inc,rpix,xpi,ypi,nvox,m,n,nz
+    real(DP) :: xp,yp,maxtdepth,excdepth,tdepth,r,depth
+    real(DP),dimension(:),allocatable :: temps,nonzero
+
+    ! Executable code
+    n = 0
+
+    maxtdepth = 0.25* (2*crater%rad)
+    excdepth = 0.5 * maxtdepth
+
+    !Find the radial extent of the transient crater in pixels
+    rpix = crater%rad / user%pix
+    inc = min(rpix,user%gridsize-1)
+
+    ! First pass to find number of voxels to average
+
+    do j=-inc,inc
+        do i=-inc,inc
+            ! find distance from crater center
+            xpi = crater%xlpx + i
+            ypi = crater%ylpx + j
+   
+            ! Find distance from crater center to current pixel center in real space
+            xp = xpi * user%pix
+            yp = ypi * user%pix
+   
+            ! periodic boundary conditions
+            call util_periodic(xpi,ypi,user%gridsize)
+
+            ! calculate the depth of the transient crater at this pixel, assuming parabolic shape:
+            r = sqrt(xp**2 + yp**2)
+            tdepth = -0.5 * (r/crater%rad)**2 + (crater%rad/2._DP)
+
+            if (tdepth > excdepth) then
+                depth = excdepth
+            else
+                depth = tdepth
+            end if
+
+            nvox = int(depth/user%zpix)
+            n = n + nvox
+        end do
+    end do
+
+    allocate(temps(n))
+    m = 0
+
+    temps(:) = 0.0_DP
+
+    ! Second pass to actually get the temperature value of each voxel that is not vapor
+
+    do j=-inc,inc
+        do i=-inc,inc
+            ! find distance from crater center
+            xpi = crater%xlpx + i
+            ypi = crater%ylpx + j
+   
+            ! Find distance from crater center to current pixel center in real space
+            xp = xpi * user%pix
+            yp = ypi * user%pix
+   
+            ! periodic boundary conditions
+            call util_periodic(xpi,ypi,user%gridsize)
+
+            ! calculate the depth of the transient crater at this pixel, assuming parabolic shape:
+            r = sqrt(xp**2 + yp**2)
+            tdepth = -0.5 * (r/crater%rad)**2 + (crater%rad/2._DP)
+
+            if (tdepth > excdepth) then
+                depth = excdepth
+            else
+                depth = tdepth
+            end if
+
+            nvox = int(depth/user%zpix)
+
+            do k=1,nvox
+                m = m + 1
+                if (thermal(xpi,ypi,k)%temperature < 3327.0) then
+                    temps(m) = thermal(xpi,ypi,k)%temperature
+                else
+                    temps(m) = 0.0_DP
+                end if
+            end do
+        end do
+    end do
+
+    ! find the number of non-zero values in the array
+
+    nz = 0
+
+    do i=1,n
+        if (temps(i) > 0.0_DP) nz = nz + 1
+    end do
+
+    allocate(nonzero(nz))
+
+    m = 0
+
+    do i=1,n
+        if (temps(i) > 0.0_DP) then
+            m = m + 1
+            nonzero(m) = temps(i)
+        end if
+    end do
+
+    ! Finally get the average
+    if (size(nonzero)>1) then
+        avgtemp = sum(nonzero) / size(nonzero)
+    else
+        avgtemp = 0.0
+    end if
+
+    deallocate(temps,nonzero)
+
+    return
+end subroutine thermal_ejecta_average
+
+
+
+
+    
