6567e62734bfaccee13261828227d44de381ac89
Ryan Wochner <rwochner@purdue.edu>
2025-10-24 13:06:43 -0400

    test message

--- src/crater/crater_populate.f90
+++ src/crater/crater_populate.f90
@@ -49,7 +49,7 @@ subroutine crater_populate(user,surf,crater,domain,prod,production_list,vdist,nt
    real(DP),dimension(:,:), intent(in)             :: rclist !array of 'real' craters for quasiMC
 
    ! Internal variables
-   real(DP)                :: cmin     ! Minimum crater diameter (m)
+   real(DP)                :: cmin     ! Minimum crater diameter (m) blahblahblah
    real(DP)                :: cmax     ! Maximum crater diameter (m)
    real(DP)                :: imin     ! Minimum impactor diameter (m)
    real(DP)                :: imax     ! Maximum impactor diameter (m)
